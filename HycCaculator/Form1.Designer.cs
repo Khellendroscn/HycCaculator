a3d0aa8789847821838bec8be83c7c3e042d111c
黄 宇超 <13819055300@163.com>
2017-10-12 15:25:59 +0800

    init

---
+++ HycCaculator/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace HycCaculator
+{
+    partial class MainWin
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btnNum7 = new System.Windows.Forms.Button();
+            this.btnNum4 = new System.Windows.Forms.Button();
+            this.btnNum1 = new System.Windows.Forms.Button();
+            this.btnNum8 = new System.Windows.Forms.Button();
+            this.btnNum9 = new System.Windows.Forms.Button();
+            this.btnNum5 = new System.Windows.Forms.Button();
+            this.btnNum6 = new System.Windows.Forms.Button();
+            this.btnNum2 = new System.Windows.Forms.Button();
+            this.btnBackSpace = new System.Windows.Forms.Button();
+            this.btnClearCur = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.btnNum0 = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.btnNum3 = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnNeg = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnCacu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(318, 38);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "0";
+            this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnNum7
+            // 
+            this.btnNum7.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum7.Location = new System.Drawing.Point(12, 152);
+            this.btnNum7.Name = "btnNum7";
+            this.btnNum7.Size = new System.Drawing.Size(75, 75);
+            this.btnNum7.TabIndex = 1;
+            this.btnNum7.Text = "7";
+            this.btnNum7.UseVisualStyleBackColor = true;
+            this.btnNum7.Click += new System.EventHandler(this.onBtnNumClick);
+            // 
+            // btnNum4
+            // 
+            this.btnNum4.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum4.Location = new System.Drawing.Point(11, 233);
+            this.btnNum4.Name = "btnNum4";
+            this.btnNum4.Size = new System.Drawing.Size(75, 75);
+            this.btnNum4.TabIndex = 1;
+            this.btnNum4.Text = "4";
+            this.btnNum4.UseVisualStyleBackColor = true;
+            this.btnNum4.Click += new System.EventHandler(this.onBtnNumClick);
+            // 
+            // btnNum1
+            // 
+            this.btnNum1.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum1.Location = new System.Drawing.Point(11, 314);
+            this.btnNum1.Name = "btnNum1";
+            this.btnNum1.Size = new System.Drawing.Size(75, 75);
+            this.btnNum1.TabIndex = 1;
+            this.btnNum1.Text = "1";
+            this.btnNum1.UseVisualStyleBackColor = true;
+            this.btnNum1.Click += new System.EventHandler(this.onBtnNumClick);
+            // 
+            // btnNum8
+            // 
+            this.btnNum8.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum8.Location = new System.Drawing.Point(93, 152);
+            this.btnNum8.Name = "btnNum8";
+            this.btnNum8.Size = new System.Drawing.Size(75, 75);
+            this.btnNum8.TabIndex = 1;
+            this.btnNum8.Text = "8";
+            this.btnNum8.UseVisualStyleBackColor = true;
+            this.btnNum8.Click += new System.EventHandler(this.onBtnNumClick);
+            // 
+            // btnNum9
+            // 
+            this.btnNum9.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum9.Location = new System.Drawing.Point(174, 152);
+            this.btnNum9.Name = "btnNum9";
+            this.btnNum9.Size = new System.Drawing.Size(75, 75);
+            this.btnNum9.TabIndex = 1;
+            this.btnNum9.Text = "9";
+            this.btnNum9.UseVisualStyleBackColor = true;
+            this.btnNum9.Click += new System.EventHandler(this.onBtnNumClick);
+            // 
+            // btnNum5
+            // 
+            this.btnNum5.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum5.Location = new System.Drawing.Point(93, 233);
+            this.btnNum5.Name = "btnNum5";
+            this.btnNum5.Size = new System.Drawing.Size(75, 75);
+            this.btnNum5.TabIndex = 1;
+            this.btnNum5.Text = "5";
+            this.btnNum5.UseVisualStyleBackColor = true;
+            this.btnNum5.Click += new System.EventHandler(this.onBtnNumClick);
+            // 
+            // btnNum6
+            // 
+            this.btnNum6.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum6.Location = new System.Drawing.Point(174, 233);
+            this.btnNum6.Name = "btnNum6";
+            this.btnNum6.Size = new System.Drawing.Size(75, 75);
+            this.btnNum6.TabIndex = 1;
+            this.btnNum6.Text = "6";
+            this.btnNum6.UseVisualStyleBackColor = true;
+            this.btnNum6.Click += new System.EventHandler(this.onBtnNumClick);
+            // 
+            // btnNum2
+            // 
+            this.btnNum2.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum2.Location = new System.Drawing.Point(92, 314);
+            this.btnNum2.Name = "btnNum2";
+            this.btnNum2.Size = new System.Drawing.Size(75, 75);
+            this.btnNum2.TabIndex = 1;
+            this.btnNum2.Text = "2";
+            this.btnNum2.UseVisualStyleBackColor = true;
+            this.btnNum2.Click += new System.EventHandler(this.onBtnNumClick);
+            // 
+            // btnBackSpace
+            // 
+            this.btnBackSpace.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnBackSpace.ForeColor = System.Drawing.Color.Red;
+            this.btnBackSpace.Location = new System.Drawing.Point(12, 71);
+            this.btnBackSpace.Name = "btnBackSpace";
+            this.btnBackSpace.Size = new System.Drawing.Size(75, 75);
+            this.btnBackSpace.TabIndex = 1;
+            this.btnBackSpace.Text = "←";
+            this.btnBackSpace.UseVisualStyleBackColor = true;
+            this.btnBackSpace.Click += new System.EventHandler(this.btnBackSpace_Click);
+            // 
+            // btnClearCur
+            // 
+            this.btnClearCur.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnClearCur.ForeColor = System.Drawing.Color.Red;
+            this.btnClearCur.Location = new System.Drawing.Point(93, 71);
+            this.btnClearCur.Name = "btnClearCur";
+            this.btnClearCur.Size = new System.Drawing.Size(75, 75);
+            this.btnClearCur.TabIndex = 1;
+            this.btnClearCur.Text = "CE";
+            this.btnClearCur.UseVisualStyleBackColor = true;
+            this.btnClearCur.Click += new System.EventHandler(this.btnClearCur_Click);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnClearAll.ForeColor = System.Drawing.Color.Red;
+            this.btnClearAll.Location = new System.Drawing.Point(174, 71);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(75, 75);
+            this.btnClearAll.TabIndex = 1;
+            this.btnClearAll.Text = "C";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // btnNum0
+            // 
+            this.btnNum0.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum0.Location = new System.Drawing.Point(12, 395);
+            this.btnNum0.Name = "btnNum0";
+            this.btnNum0.Size = new System.Drawing.Size(75, 75);
+            this.btnNum0.TabIndex = 1;
+            this.btnNum0.Text = "0";
+            this.btnNum0.UseVisualStyleBackColor = true;
+            this.btnNum0.Click += new System.EventHandler(this.onBtnNumClick);
+            // 
+            // btnDot
+            // 
+            this.btnDot.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnDot.Location = new System.Drawing.Point(93, 395);
+            this.btnDot.Name = "btnDot";
+            this.btnDot.Size = new System.Drawing.Size(75, 75);
+            this.btnDot.TabIndex = 1;
+            this.btnDot.Text = ".";
+            this.btnDot.UseVisualStyleBackColor = true;
+            this.btnDot.Click += new System.EventHandler(this.btnDot_Click);
+            // 
+            // btnNum3
+            // 
+            this.btnNum3.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNum3.Location = new System.Drawing.Point(173, 314);
+            this.btnNum3.Name = "btnNum3";
+            this.btnNum3.Size = new System.Drawing.Size(75, 75);
+            this.btnNum3.TabIndex = 1;
+            this.btnNum3.Text = "3";
+            this.btnNum3.UseVisualStyleBackColor = true;
+            this.btnNum3.Click += new System.EventHandler(this.onBtnNumClick);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnDiv.ForeColor = System.Drawing.Color.Red;
+            this.btnDiv.Location = new System.Drawing.Point(255, 71);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(75, 75);
+            this.btnDiv.TabIndex = 1;
+            this.btnDiv.Text = "÷";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.onBtnSignClick);
+            // 
+            // btnNeg
+            // 
+            this.btnNeg.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNeg.ForeColor = System.Drawing.Color.Blue;
+            this.btnNeg.Location = new System.Drawing.Point(173, 395);
+            this.btnNeg.Name = "btnNeg";
+            this.btnNeg.Size = new System.Drawing.Size(75, 75);
+            this.btnNeg.TabIndex = 1;
+            this.btnNeg.Text = "+/-";
+            this.btnNeg.UseVisualStyleBackColor = true;
+            this.btnNeg.Click += new System.EventHandler(this.btnNeg_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnPlus.ForeColor = System.Drawing.Color.Red;
+            this.btnPlus.Location = new System.Drawing.Point(254, 314);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 75);
+            this.btnPlus.TabIndex = 1;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.onBtnSignClick);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnMultiply.ForeColor = System.Drawing.Color.Red;
+            this.btnMultiply.Location = new System.Drawing.Point(255, 152);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 75);
+            this.btnMultiply.TabIndex = 1;
+            this.btnMultiply.Text = "×";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.onBtnSignClick);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSub.ForeColor = System.Drawing.Color.Red;
+            this.btnSub.Location = new System.Drawing.Point(254, 233);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(75, 75);
+            this.btnSub.TabIndex = 1;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.onBtnSignClick);
+            // 
+            // btnCacu
+            // 
+            this.btnCacu.Font = new System.Drawing.Font("黑体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCacu.ForeColor = System.Drawing.Color.Red;
+            this.btnCacu.Location = new System.Drawing.Point(255, 395);
+            this.btnCacu.Name = "btnCacu";
+            this.btnCacu.Size = new System.Drawing.Size(75, 75);
+            this.btnCacu.TabIndex = 1;
+            this.btnCacu.Text = "=";
+            this.btnCacu.UseVisualStyleBackColor = true;
+            this.btnCacu.Click += new System.EventHandler(this.onBtnCacuClick);
+            // 
+            // MainWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 482);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnNum3);
+            this.Controls.Add(this.btnCacu);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnNum6);
+            this.Controls.Add(this.btnDot);
+            this.Controls.Add(this.btnNum2);
+            this.Controls.Add(this.btnNeg);
+            this.Controls.Add(this.btnNum5);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnNum9);
+            this.Controls.Add(this.btnClearCur);
+            this.Controls.Add(this.btnNum8);
+            this.Controls.Add(this.btnNum0);
+            this.Controls.Add(this.btnNum1);
+            this.Controls.Add(this.btnNum4);
+            this.Controls.Add(this.btnBackSpace);
+            this.Controls.Add(this.btnNum7);
+            this.Controls.Add(this.textBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainWin";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btnNum7;
+        private System.Windows.Forms.Button btnNum4;
+        private System.Windows.Forms.Button btnNum1;
+        private System.Windows.Forms.Button btnNum8;
+        private System.Windows.Forms.Button btnNum9;
+        private System.Windows.Forms.Button btnNum5;
+        private System.Windows.Forms.Button btnNum6;
+        private System.Windows.Forms.Button btnNum2;
+        private System.Windows.Forms.Button btnBackSpace;
+        private System.Windows.Forms.Button btnClearCur;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Button btnNum0;
+        private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.Button btnNum3;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnNeg;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnCacu;
+    }
+}
+
---
+++ HycCaculator/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HycCaculator
+{
+    public partial class MainWin : Form
+    {
+        private combine<double> funcBuffer;
+        private double numBuffer = 0;
+        bool hasDot = false;
+        bool inputComplete = false;
+
+        // 表示二元谓词
+        private delegate double combine<T>(T a, T b);
+
+        //将操作符的名称映射到相应的函数
+        private static Dictionary<char, combine<double>> operatorDict;
+
+        static MainWin()
+        {
+            operatorDict = new Dictionary<char, combine<double>>();
+            operatorDict['+'] = (a, b) => a + b;
+            operatorDict['-'] = (a, b) => a - b;
+            operatorDict['×'] = (a, b) => a * b;
+            operatorDict['÷'] = (a, b) => a / b;
+        }
+
+        public MainWin()
+        {
+            InitializeComponent();
+        }
+        
+        private void onBtnNumClick(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            string text = b.Text;
+            string num = textBox.Text;
+            inputNum(text);
+        }
+
+        private void inputNum(string num)
+        {
+            if (inputComplete || textBox.Text.Equals("0"))
+            {
+                textBox.Text = num;
+                inputComplete = false;
+            }
+            else
+            {
+                textBox.Text += num;
+            }
+        }
+
+        private void btnNeg_Click(object sender, EventArgs e)
+        {
+            string text = textBox.Text;
+            double num = double.Parse(text);
+            if (!inputComplete && num != 0)
+            {
+                num = -num;
+                textBox.Text = num.ToString();
+            }
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            inputDot();
+        }
+
+        private void inputDot()
+        {
+            if (inputComplete)
+            {
+                inputNum("0");
+                inputDot();
+                return;
+            }
+            if (!hasDot)
+            {
+                textBox.Text += '.';
+                hasDot = true;
+            }
+        }
+
+        private void btnBackSpace_Click(object sender, EventArgs e)
+        {
+            string text = textBox.Text;
+            if (text.Length > 1)
+            {
+                int index = text.Length - 1;
+                char delChar = text[index];
+                if (delChar == '.') { hasDot = false; }
+                textBox.Text = text.Remove(index, 1);
+            }
+            else
+            {
+                textBox.Text = "0";
+            }
+        }
+
+        private void onBtnSignClick(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            char sign = btn.Text[0];
+            if (funcBuffer != null)
+            {// 如果存在尚未结算的操作，则先结算该操作
+                cacu();
+            }
+            funcBuffer = operatorDict[sign];// 根据运算符号查找相应的函数，然后等待用户输入右操作数
+            setInputComplete();
+        }
+        private void setInputComplete()
+        {
+            if (!inputComplete)
+            {
+                inputComplete = true;
+                hasDot = false;
+                numBuffer = Double.Parse(textBox.Text);
+            }
+        }
+        private void cacu()
+        {
+            if (funcBuffer != null)
+            {
+                var numRight = Double.Parse(textBox.Text);
+                var result = funcBuffer(numBuffer, numRight);
+                textBox.Text = result.ToString();
+                funcBuffer = null;
+            }
+        }
+        private void onBtnCacuClick(object sender, EventArgs e)
+        {
+            cacu();
+            setInputComplete();
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "0";
+            numBuffer = 0;
+            funcBuffer = null;
+            inputComplete = false;
+            hasDot = false;
+        }
+
+        private void btnClearCur_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "0";
+        }
+
+    }
+}
